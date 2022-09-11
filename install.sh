5b63fd733cf17cbcd6743bdc62c1e9b1d0d56dde
Aleksandr Mospan <alexander.mospan@semrush.com>
2022-09-11 19:05:50 +0200

    fix icons bug that caused a crash

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ev
 
+iconutil --convert icns --output clean-full-ansi/Contents/Resources/English\ -\ clean-full-ansi.icns English.iconset
+iconutil --convert icns --output clean-full-ansi/Contents/Resources/Russian\ -\ clean-full-ansi.icns Russian.iconset
+
 cp -R clean-full-ansi clean-full-ansi.bundle
 
-rm -rf "~/Library/Keyboard\ Layouts/clean-full-ansi.bundle"
+rm -rf ~/Library/Keyboard\ Layouts/clean-full-ansi.bundle
 mv clean-full-ansi.bundle ~/Library/Keyboard\ Layouts/
