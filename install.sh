43177824b1a4f6f99bef7c4552481c84aefce4e0
Aleksandr Mospan <alexander.mospan@semrush.com>
2022-09-11 19:51:59 +0200

    fix layout and install script

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -ev
 
-iconutil --convert icns --output clean-full-ansi/Contents/Resources/English\ -\ clean-full-ansi.icns English.iconset
-iconutil --convert icns --output clean-full-ansi/Contents/Resources/Russian\ -\ clean-full-ansi.icns Russian.iconset
-
-cp -R clean-full-ansi clean-full-ansi.bundle
+iconutil --convert icns --output clean-full-ansi.bundle/Contents/Resources/English\ -\ clean-full-ansi.icns English.iconset
+iconutil --convert icns --output clean-full-ansi.bundle/Contents/Resources/Russian\ -\ clean-full-ansi.icns Russian.iconset
 
 rm -rf ~/Library/Keyboard\ Layouts/clean-full-ansi.bundle
-mv clean-full-ansi.bundle ~/Library/Keyboard\ Layouts/
+cp -R clean-full-ansi.bundle ~/Library/Keyboard\ Layouts/
