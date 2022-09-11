05932ef109932d9f31c52996207ef8c151a54aa3
Aleksandr Mospan <alexander.mospan@semrush.com>
2022-09-11 17:47:53 +0200

    keylayout first test version

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-cp --recursive clean-full-ansi clean-full-ansi.bundle
+cp -R clean-full-ansi clean-full-ansi.bundle
 
-sudo rm --recursive --force ~/Library/Keyboard\ Layouts/clean-full-ansi.bundle
-sudo mv clean-full-ansi.bundle ~/Library/Keyboard\ Layouts/
+rm -rf "~/Library/Keyboard\ Layouts/clean-full-ansi.bundle"
+mv clean-full-ansi.bundle ~/Library/Keyboard\ Layouts/
