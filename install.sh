99b100aebd145593ef6bcf1dffa122d7ca1765be
Aleksandr Mospan <alexander.mospan@semrush.com>
2023-01-24 14:20:15 +0100

    returned numbers button to initial place

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-iconutil --convert icns --output clean-full-ansi.bundle/Contents/Resources/English\ -\ clean-full-ansi.icns English.iconset
-iconutil --convert icns --output clean-full-ansi.bundle/Contents/Resources/Russian\ -\ clean-full-ansi.icns Russian.iconset
+# iconutil --convert icns --output clean-full-ansi.bundle/Contents/Resources/English\ -\ clean-full-ansi.icns English.iconset
+# iconutil --convert icns --output clean-full-ansi.bundle/Contents/Resources/Russian\ -\ clean-full-ansi.icns Russian.iconset
 
 rm -rf ~/Library/Keyboard\ Layouts/clean-full-ansi.bundle
 cp -R clean-full-ansi.bundle ~/Library/Keyboard\ Layouts/
