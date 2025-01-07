a44328a87c9d2bb2e4e60831291a8fdabece9cb1
Aleksandr Mospan <alexander.mospan@gmail.com>
2025-01-07 16:27:36 +0300

    cleanup

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -ev
 
-iconutil --convert icns --output mospan-keyboard-layout.bundle/Contents/Resources/English\ -\ mospan-keyboard-layout.icns English.iconset
-iconutil --convert icns --output mospan-keyboard-layout.bundle/Contents/Resources/Russian\ -\ mospan-keyboard-layout.icns Russian.iconset
+iconutil --convert icns --output mospan-keyboard-layout.bundle/Contents/Resources/English\ —\ Mospan.icns English.iconset
+iconutil --convert icns --output mospan-keyboard-layout.bundle/Contents/Resources/Russian\ —\ Mospan.icns Russian.iconset
 
 rm -rf ~/Library/Keyboard\ Layouts/mospan-keyboard-layout.bundle
 cp -R mospan-keyboard-layout.bundle ~/Library/Keyboard\ Layouts/
